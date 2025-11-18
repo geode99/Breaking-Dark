04748339b5180e7801dc35d7fd6b91432a01c329
MUKILTEO\1725489 <kayla.gerlach42@gmail.com>
2025-11-18 10:23:23 -0800

    switching between players set up

---
+++ Assets/kayla/scripts/ExitGame.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ExitGame : MonoBehaviour
+{ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.ExitPlaymode(); 
+#endif 
+        } 
+    } 
+} 
--- Assets/kayla/scripts/PlayerMovement.cs
+++ Assets/kayla/scripts/PlayerMovement.cs
@@ -4,28 +4,52 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5.0f;
-    public float jumpForce = 10.0f;
-    private Rigidbody2D rb2d;
+    private float jumpForce;
+    public float wizardJumpForce = 15.0f;
+    public float fireflyJumpForce = 8.0f;
     private float _movement;
+    private bool tab = false;
+
+    private Rigidbody2D rb2d;
+    public Rigidbody2D firefly;
+    public Rigidbody2D wizard;
+
+    private SpriteRenderer playerSR;
+    public SpriteRenderer wizardSR;
+    public SpriteRenderer fireflySR;
+
+    private bool isWizard = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = wizard;
+        playerSR = wizardSR;
+        jumpForce = wizardJumpForce;
     }
 
     // Update is called once per frame
     void Update()
     {
         rb2d.linearVelocityX = _movement;
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput > 0)
+        {
+            playerSR.flipX = false;
+        }
+        else if (horizontalInput < 0)
+        {
+            playerSR.flipX = true;
+        }
     }
 
     public void Move(InputAction.CallbackContext ctx)
     {
-        _movement = ctx.ReadValue<Vector2>().x *speed; 
+        _movement = ctx.ReadValue<Vector2>().x * speed;
     }
     public void Jump(InputAction.CallbackContext ctx)
     {
-        if (ctx.ReadValue<float>() ==1)
+        if (ctx.ReadValue<float>() == 1)
         {
             rb2d.linearVelocityY = jumpForce;
         }
@@ -35,7 +59,30 @@ public class PlayerMovement : MonoBehaviour
     {
         if (ctx.ReadValue<float>() == 1)
         {
+            tab = !tab;
             Debug.Log("switched");
+            if (tab)
+            {
+                rb2d = wizard;
+                playerSR = wizardSR;
+                jumpForce = wizardJumpForce;
+                isWizard = true;
+            }
+            else if (tab == false)
+            {
+                rb2d = firefly;
+                playerSR = fireflySR;
+                jumpForce = fireflyJumpForce;
+                isWizard = false;
+            }
+        }
+    }
+
+    public void Down(InputAction.CallbackContext ctx)
+    {
+        if (ctx.ReadValue<float>() == 1 && isWizard == false)
+        {
+            rb2d.linearVelocityY = -jumpForce;
         }
     }
 }
