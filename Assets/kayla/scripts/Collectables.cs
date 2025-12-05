0b995f9a0dcefa440392cea51f9601b83ca1ce99
MUKILTEO\1725489 <kayla.gerlach42@gmail.com>
2025-12-05 08:44:48 -0800

    fixed text fade

--- Assets/kayla/scripts/Collectables.cs
+++ Assets/kayla/scripts/Collectables.cs
@@ -2,21 +2,26 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class Collectables : MonoBehaviour
 {
     public TextMeshProUGUI collectableText;
     private Color startColor;
-    private Color endColor;
+    //private Color endColor;
     private Color resetColor;
     public float fadeDuration = 2f;
-    float timer = 0f;
+    public List<string> collectedItems = new List<string>();
+    public string itemName;
+    //float timer = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         collectableText.enabled = false;
         startColor = collectableText.color;
-        endColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+        //endColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
         resetColor = new Color(startColor.r, startColor.g, startColor.b, 1f);
         collectableText.color = resetColor;
     }
@@ -27,16 +32,21 @@ public class Collectables : MonoBehaviour
         
     }
 
+    public void FadeOut()
+    {
+        collectableText.CrossFadeAlpha(1f, 0f, false);
+        collectableText.CrossFadeAlpha(0f, fadeDuration, false);
+    }
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             collectableText.enabled = true;
-            timer += Time.deltaTime;
-            float t = timer / fadeDuration;
-            collectableText.color = Color.Lerp(startColor, endColor, t);
-            collectableText.enabled = false;
             Destroy(gameObject);
+            FadeOut();
+            collectableText.color = resetColor;
+            collectedItems.Add(itemName);
         }
     }
 }
