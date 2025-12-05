a2afeaaa4d7d8447c6d86e06abe9e39aecf1238f
MUKILTEO\1725489 <kayla.gerlach42@gmail.com>
2025-12-05 09:33:21 -0800

    started animations

--- Assets/kayla/scripts/HouseTriggerZone.cs
+++ Assets/kayla/scripts/HouseTriggerZone.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using static UnityEditor.Rendering.CoreEditorDrawer<TData>;
 
 public class HouseTriggerZone : MonoBehaviour
 {
@@ -9,15 +11,19 @@ public class HouseTriggerZone : MonoBehaviour
     public HouseControls HouseControlsReference;
     public bool inHouse = false;
     public Canvas houseCanvas;
+    public Collectables collectablesReference;
+    public List<string> collectedItems;
+    public List<GameObject> collectedObjects;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        collectedItems = collectablesReference.collectedItems;
         M = HouseControlsReference.M;
         D = HouseControlsReference.D;
         if (M && D && inHouseZone)
@@ -25,8 +31,19 @@ public class HouseTriggerZone : MonoBehaviour
             Debug.Log("In House Zone and M pressed");
             houseCanvas.enabled = true;
             inHouse = true;
+            //ShowCollection();
         }
     }
+
+    //private void ShowCollection()
+   // {
+        //Debug.Log("Collected Items:");
+        //foreach (string item in collectedItems)
+        //{
+            //Debug.Log(item);
+            //GameObject collectedObjects<item>.enabled = true;
+        //}
+    //}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
