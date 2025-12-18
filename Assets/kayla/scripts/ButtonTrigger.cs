fcfd1db41a821716b0ba22eb287e8e2750582015
DESKTOP-T5FTG4L\kayla <kayla.gerlach42@gmail.com>
2025-12-17 17:22:03 -0800

    button and lantern trigger + more lvl 3 work

---
+++ Assets/kayla/scripts/ButtonTrigger.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public BoxCollider2D door;
+    public SpriteRenderer doorSR;
+
+    private Light2D flame;
+    public PlayerMovement playerMovementReference;
+    private bool isFireflyOn = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        flame = GetComponent<Light2D>();
+        flame.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isFireflyOn = playerMovementReference.isFireflyOn;
+    }
+
+
+    public void LanternOn()
+    {
+        if (isFireflyOn)
+        {
+            door.isTrigger = true;
+            doorSR.color = new Color(1f, 1f, 1f, 0.3f);
+            flame.enabled = true;
+        }
+    }
+
+    public void ButtonOn()
+    {
+        door.isTrigger = true;
+        doorSR.color = new Color(1f, 1f, 1f, 0.3f);
+    }
+}
